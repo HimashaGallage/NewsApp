3d1209ee98a182838dde879e4a857070a613757f
HimashaGallage <gallagehh@gmail.com>
2024-12-17 00:43:40 +0530

    scroll to top when reload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import NewsFeed from './screens/NewsFeed/NewsFeed';
 import Header from './components/Header';
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="container mx-auto p-4 bg-primary">
       <Header />
